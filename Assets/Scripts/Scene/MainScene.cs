f530dda9c33dcaeb27a604e361297d5c02aa3592
CastleJH <sjh000319@naver.com>
2022-11-26 21:13:52 +0900

    애니메이션

--- Assets/Scripts/Scene/MainScene.cs
+++ Assets/Scripts/Scene/MainScene.cs
@@ -57,7 +57,6 @@ public class MainScene : BaseScene
     //퀘스트들을 만들고 첫번째 비버 입장
     void StartStage()
     {
-        Debug.Log(BeaverGameManager.Instance.GetCurrScene<MainScene>());
         currentStage++;
 
         currentQuestIdx = -1;
